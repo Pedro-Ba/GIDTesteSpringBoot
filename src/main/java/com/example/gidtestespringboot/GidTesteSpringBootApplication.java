849d0711813ed276aa5f8e6f025e927d268c8168
duartella <rafaelladuartecb@gmail.com>
2025-04-14 19:09:10 -0300

    Initial commit

---
+++ src/main/java/com/example/gidtestespringboot/GidTesteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gidtestespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GidTesteSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GidTesteSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gidtestespringboot/GidTesteSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gidtestespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GidTesteSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
